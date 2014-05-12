09fc012efc1b41a05844211cadd87ce36a39fcef
Kasidis Arunruangsirilert <kken62@gmail.com>
2014-05-12 16:26:13 +0700

    Update Add Project

--- app/scripts/controllers/addproject.js
+++ app/scripts/controllers/addproject.js
@@ -1,10 +1,24 @@
 'use strict';
 
 angular.module('firstNgAppApp')
-  .controller('AddprojectCtrl', function ($scope) {
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate',
-        'AngularJS',
-        'Karma'
-    ];
+.controller('AddprojectCtrl', function ($scope,contacts) {
+	contacts.$bind($scope, 'contacts');
+
+	//$scope.contacts = contacts;
+
+	$scope.add = function(input){
+		$scope.contacts.$add({
+			name:input.name,
+			tel: input.tel,
+			email:input.email,
+			bird:input.bird,
+			status:1
+		});
+
+		console.log($scope.contacts);
+	};
+
+	$scope.delete = function(contact){
+		contact.status = 0;
+	};
 });
