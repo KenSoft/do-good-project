fb918779f5ca74d00407b40472518907c69eb027
Kasidis Arunruangsirilert <kken62@gmail.com>
2014-05-13 09:44:11 +0700

    Change Backend to Parse

--- app/scripts/services/contacts.js
+++ app/scripts/services/contacts.js
@@ -2,7 +2,93 @@
 'use strict';
 
 angular.module('firstNgAppApp')
-.factory('contacts', ['$firebase', function($firebase) {
-    var ref = new Firebase('https://do-good-project.firebaseio.com/contacts');
-    return $firebase(ref);
+.factory('Parse', function ($http) {
+    var app_id = 'syN64QMWju6KQhFZYxbgIvMNfMgKq2DkVb6cHYEi';
+    var api_key = 'HEVdhYLuFSEWHC86GbMVQyPkWziTKGmgwJ2ioF9C';
+    console.log("Factory");
+    return {
+        searchHot : function(q,callback){
+          console.log(q);
+            $http({
+                 params: {
+            where: {barcode:q}
+                },
+            method: 'GET',
+                url:'https://api.parse.com/1/classes/hotDeal',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                callback(data);
+            });
+        },
+         searchTel : function(q,callback){
+          console.log(q);
+            $http({
+                 params: {
+            where: {barcode:q}
+                },
+            method: 'GET',
+                url:'https://api.parse.com/1/classes/products',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                callback(data);
+            });
+        },
+           searchName : function(q,callback){
+            console.log(q);
+            $http({
+                 params: {
+            where: {productName:q}
+                },
+            method: 'GET',
+                url:'https://api.parse.com/1/classes/products',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                callback(data);
+            });
+        },
+        getAll : function(callback){
+          console.log("GetAll");
+            $http({
+                method: 'GET',
+                url:'https://api.parse.com/1/classes/hotDeal',
+                headers:
+                {
+                    'X-Parse-Application-Id': app_id,
+                    'X-Parse-REST-API-Key': api_key,
+                    'Content-Type': 'application/json'
+                }
+            })
+            .success(function(data) {
+                console.log(data);
+                console.log("success");
+                callback(data);
+            })
+            .error(function(data){
+                console.log("error");
+            });
+
+        }
+    };
+
 }]);
