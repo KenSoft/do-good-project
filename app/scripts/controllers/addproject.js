1177c1be70fb961493e37ef92898ca0582554758
Kasidis Arunruangsirilert <kken62@gmail.com>
2014-05-14 11:38:46 +0700

    Change To Parse #2

--- app/scripts/controllers/addproject.js
+++ app/scripts/controllers/addproject.js
@@ -1,24 +1,9 @@
 'use strict';
 
 angular.module('firstNgAppApp')
-.controller('AddprojectCtrl', function ($scope,contacts) {
-	contacts.$bind($scope, 'contacts');
-
-	//$scope.contacts = contacts;
+.controller('AddprojectCtrl', function ($scope,Contacts) {
 
 	$scope.add = function(input){
-		$scope.contacts.$add({
-			name:input.name,
-			tel: input.tel,
-			email:input.email,
-			bird:input.bird,
-			status:1
-		});
-
-		console.log($scope.contacts);
-	};
-
-	$scope.delete = function(contact){
-		contact.status = 0;
+		
 	};
 });
--- app/scripts/services/contacts.js
+++ app/scripts/services/contacts.js
@@ -1,8 +1,91 @@
-/* global Firebase */
 'use strict';
 
 angular.module('firstNgAppApp')
-.factory('contacts', ['$firebase', function($firebase) {
-    var ref = new Firebase('https://do-good-project.firebaseio.com/contacts');
-    return $firebase(ref);
+.factory('Contacts', ['$http', function($http) {
+	var appId	= 'syN64QMWju6KQhFZYxbgIvMNfMgKq2DkVb6cHYEi';
+	var apiKey	= 'HEVdhYLuFSEWHC86GbMVQyPkWziTKGmgwJ2ioF9C';
+
+	return {
+		searchHot : function(q,callback){
+			console.log(q);
+			$http({
+				params: {
+					where: {barcode:q}
+				},
+				method: 'GET',
+				url:'https://api.parse.com/1/classes/hotDeal',
+				headers:
+				{
+					'X-Parse-Application-Id': appId,
+					'X-Parse-REST-API-Key': apiKey,
+					'Content-Type': 'application/json'
+				}
+			})
+			.success(function(data) {
+				console.log(data);
+				callback(data);
+			});
+		},
+		searchTel : function(q,callback){
+			console.log(q);
+			$http({
+				params: {
+					where: {barcode:q}
+				},
+				method: 'GET',
+				url:'https://api.parse.com/1/classes/products',
+				headers:
+				{
+					'X-Parse-Application-Id': appId,
+					'X-Parse-REST-API-Key': apiKey,
+					'Content-Type': 'application/json'
+				}
+			})
+			.success(function(data) {
+				console.log(data);
+				callback(data);
+			});
+		},
+		searchName : function(q,callback){
+			console.log(q);
+			$http({
+				params: {
+					where: {productName:q}
+				},
+				method: 'GET',
+				url:'https://api.parse.com/1/classes/products',
+				headers:
+				{
+					'X-Parse-Application-Id': appId,
+					'X-Parse-REST-API-Key': apiKey,
+					'Content-Type': 'application/json'
+				}
+			})
+			.success(function(data) {
+				console.log(data);
+				callback(data);
+			});
+		},
+		getAll : function(callback){
+			console.log('GetAll');
+			$http({
+				method: 'GET',
+				url:'https://api.parse.com/1/classes/hotDeal',
+				headers:
+				{
+					'X-Parse-Application-Id': appId,
+					'X-Parse-REST-API-Key': apiKey,
+					'Content-Type': 'application/json'
+				}
+			})
+			.success(function(data) {
+				console.log(data);
+				console.log('success');
+				callback(data);
+			})
+			.error(function(data){
+				console.log('error'+data);
+			});
+		}
+	};
 }]);
