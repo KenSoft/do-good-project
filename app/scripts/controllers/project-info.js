52be5c76f79a44b2d62dbc694f7ecb2847946ae1
Kasidis Arunruangsirilert <kken62@gmail.com>
2014-05-15 12:12:10 +0700

    Add Project Info

---
+++ app/scripts/controllers/project-info.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('firstNgAppApp')
+  .controller('ProjectInfoCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
