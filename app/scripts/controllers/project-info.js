3574a6a08d2693657dc10107c9bfe575e0fc664f
Kasidis Arunruangsirilert <kken62@gmail.com>
2014-05-15 12:40:59 +0700

    Add Comment to Project Info

--- app/scripts/controllers/project-info.js
+++ app/scripts/controllers/project-info.js
@@ -6,5 +6,26 @@ angular.module('firstNgAppApp')
 	Contacts.getInfo($routeParams.projectId,function(data){
 		console.log(data.results);
 		$scope.detail=data.results[0];
+		console.log($routeParams.projectId);
+		Contacts.getAllComment($routeParams.projectId,function(data){
+			console.log(data);
+			$scope.comments=data.results;
+			console.log($scope.comments);
 	});
+	$scope.getDate=function(object){
+		var date=new Date(object.iso);
+		console.log(date);
+		return date.toDateString();
+	};
+	$scope.add = function(comment,project){
+		console.log('working');
+		Contacts.addComment(comment,project,function(data){
+			console.log(data);
+			
+
+		});
+		window.location.reload();
+	};
+
+});
 });
